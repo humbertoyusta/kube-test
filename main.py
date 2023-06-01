d7c495b5f29b874c9b5b12666fa81a9c7470a063
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-06-01 22:56:49 +0200

    adding log volume

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import logging
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+logging.basicConfig(filename='/app/logs/main.log', level=logging.DEBUG)
+
 @app.route('/api')
 def hello():
+    app.logger.info('API route was accessed')
     return jsonify(
         name="Hello",
         description="Humberto",
